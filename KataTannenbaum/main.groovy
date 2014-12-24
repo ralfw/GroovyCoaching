68b14ac9eeca8f262cbde7ff4072b5ad68ba0ce6
Ralf Westphal <info@ralfw.de>
2014-12-24 13:13:10 +0100

    Jetzt aber Kata Tannenbaum

---
+++ KataTannenbaum/main.groovy
@@ -0,0 +1,7 @@
+/**
+ * Created by ralfw on 24.12.14.
+ * Coding Dojo der CCD SchooL: http://ccd-school.de/coding-dojo/
+ * Kata-Beschreibung: https://app.box.com/files/0/f/885439646/1/f_15903236703
+ */
+
+
